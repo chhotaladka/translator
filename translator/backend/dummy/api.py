cd316fe58a71cb9162f028a5acf753cfb6a48179
Anshul Thakur <athakur@cdot.in>
2019-07-23 10:37:27 +0530

    REST backend for google translation API implemented. Test cases to follow.

---
+++ translator/backend/dummy/api.py
@@ -0,0 +1,10 @@
+
+class Client(object):
+  def __init__(self, key=None):
+    self.key = key
+
+
+  def translate(self, text=None, src_lang='en', dst_lang='hi'):
+    if text is not None and len(text.strip()) > 0:
+      translation = 'This is a dummy module!'
+      return (translation)
