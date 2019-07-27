5798688e6e7a7a5f40a6a0d6387196668e2d91dc
Anshul Thakur <anshulthakur@rediffmail.com>
2019-07-27 10:07:37 +0530

    Frontend and some other code for saving translations.

--- translator/models.py
+++ translator/models.py
@@ -8,13 +8,23 @@ LANGUAGES = [('EN', 'English'),
     
 class Wordlist(models.Model):
     source = models.TextField(blank=False, null=False)
-    src_lang = models.CharField(options = LANGUAGES, blank=False, default='EN')
+    src_lang = models.CharField(choices = LANGUAGES, 
+                                blank=False, 
+                                default='EN',
+                                max_length=2)
     
-    target = models.ForeignKey("Translations", null=False)
+    target = models.ForeignKey("Translations", 
+                               null=False, 
+                               on_delete=models.DO_NOTHING)
 
 
 class Translations(models.Model):
     source = models.TextField(blank=False, null=False)
-    src_lang = models.CharField(options = LANGUAGES, blank=False, default='EN')
+    src_lang = models.CharField(choices = LANGUAGES, 
+                                blank=False, 
+                                default='EN',
+                                max_length=2)
     
-    target = models.ForeignKey("Translations", null=False)
+    target = models.ForeignKey("Translations", 
+                               null=False,
+                               on_delete=models.DO_NOTHING)
