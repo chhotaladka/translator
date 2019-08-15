47c8619c8d60951aba54149007643f4b33131144
Anshul Thakur <anshulthakur@rediffmail.com>
2019-08-15 10:15:03 +0530

    Some changes to have multiple selenium instances running to handle many requests.

--- translator/backend/google-selenium-asyncio/api.py
+++ translator/backend/google-selenium-asyncio/api.py
@@ -3,27 +3,8 @@ import traceback
 import time
 import os
 
-import asyncio
-
-class ClientProtocol(asyncio.Protocol):
-  def __init__(self, client, message, loop):
-    self.message = message
-    self.loop = loop
-    self.client = client
-
-  def connection_made(self, transport):
-    transport.write(self.message.encode())
-    print('Data sent: {!r}'.format(self.message))
-
-  def data_received(self, data):
-    print('Data received: {!r}'.format(data.decode()))
-    self.client.translation = data.decode()
-
-  def connection_lost(self, exc):
-    print('The server closed the connection')
-    print('Stop the event loop')
-    self.loop.stop()
-
+import socket
+HOST,PORT = '127.0.0.1', 8888
 
 class Client(object):
   def __init__(self, key=None):
@@ -32,12 +13,9 @@ class Client(object):
 
   def translate(self, text=None, src_lang='en', dst_lang='hi'):
     if text is not None and len(text.strip()) > 0:
-      loop = asyncio.new_event_loop()
-      #loop = asyncio.get_event_loop()
-      asyncio.set_event_loop(loop)
-      coro = loop.create_connection(lambda: ClientProtocol(self, text.strip(), loop), '127.0.0.1', 8888)
-
-      loop.run_until_complete(coro)
-      loop.run_forever()
-      loop.close()
+      with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        sock.sendall(text.strip().encode())
+        self.translation = sock.recv(1024).decode()
+        sock.close()
     return (self.translation)
--- translator/backend/google-selenium-asyncio/server.py
+++ translator/backend/google-selenium-asyncio/server.py
@@ -8,24 +8,49 @@ import time
 import os
 
 import signal
+import functools
 
-loop = None
-driver = None
+
 server = None
 
-def handler(signum, frame):
-  print('Shutting down...')
-  if driver is not None:
-    driver.close()
-    driver.quit()
-  if server is not None and loop is not None:
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
-  import sys
-  sys.exit(0)
 
-def translate(driver, text=None):
+from queue import Empty, Queue
+from threading import Thread
+
+num_threads = 2
+
+q = None
+workers = []
+
+def shutdown():
+  global q
+  global workers
+  global server
+
+  print('Shutting down...')
+  if len(workers)>0:
+      for i in range(num_threads):
+        q.put('stop')
+      try:
+        while True:
+            worker_thread = workers.pop()
+            worker_thread.join()
+      except IndexError:
+        pass
+
+
+  if server is not None:
+    server.close()
+    server = None
+    #loop.stop()
+    #loop.wait_closed()
+    #loop.close()
+
+def handler(signum):
+    print('Signal handled.')
+    shutdown()
+
+def translate(driver, text=None, wait=0.9):
     if text is not None and len(text.strip()) > 0:
       translation = ''
       try:
@@ -33,7 +58,7 @@ def translate(driver, text=None):
         if src is not None:
           src.clear();
           src.send_keys(text)
-          time.sleep(0.9)
+          time.sleep(wait)
           dest = driver.find_element_by_css_selector(".tlid-translation.translation")
           children = dest.find_elements_by_css_selector("*")
           for child in children:
@@ -41,16 +66,18 @@ def translate(driver, text=None):
               translation += child.text
             elif (child.tag_name).lower() == 'br':
               translation += '\n'
+        retry=False
       except:
           traceback.print_exc()
+          retry = True
       #print(translation)
-      return (translation)
+      return (translation, retry)
 
 
 class EchoServerClientProtocol(asyncio.Protocol):
-  def __init__(self, driver, *args, **kwargs):
+  def __init__(self, queue, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.driver = driver
+    self.queue = queue
     self.terminate = False
 
   def connection_made(self, transport):
@@ -59,58 +86,96 @@ class EchoServerClientProtocol(asyncio.Protocol):
     self.transport = transport
 
   def data_received(self, data):
-    message = data.decode()
-    print('Data received: {!r}'.format(message))
+    print('data received')
+    self.message = data.decode()
+    print('Data received: {!r}'.format(self.message))
+    self.translation = ''
+    self.queue.put(self)
 
-    translation = translate(self.driver, message)
-
-    print('Send: {!r}'.format(translation))
-    self.transport.write(translation.encode('utf-8'))
+  def send_response(self):
+    print('Send: {!r}'.format(self.translation))
+    self.transport.write(self.translation.encode('utf-8'))
 
     print('Close the client socket')
     self.transport.close()
 
+def worker(name, queue):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    #chrome_options.binary_location='/opt/google/chrome/google-chrome'
+    try:
+        driver = webdriver.Chrome(os.path.dirname(os.path.abspath(__file__))+"/../resources/chromedriver", chrome_options=chrome_options)
+        driver.get("https://translate.google.com/#view=home&op=translate&sl=en&tl=hi")
+
+        while True:
+            try:
+                work = queue.get(timeout=0.1)
+                if isinstance(work, EchoServerClientProtocol):
+                    (work.translation, retry )= translate(driver, work.message, wait=0.9)
+                    work.send_response()
+                elif isinstance(work, str):
+                    if work=='stop':
+                        break;
+                queue.task_done()
+            except Empty:
+                pass
+
+        print('Closing {a}'.format(a=name))
+        #driver.close()
+        driver.quit()
+
+    except:
+        traceback.print_exc()
+
+
 def main():
   global loop
   global server
-  global driver
+  global coro
+  global q
+  global workers
 
-  signal.signal(signal.SIGTERM, handler)
+  try:
+    q = Queue(num_threads)
 
-  chrome_options = Options()
-  chrome_options.add_argument('--headless')
-  #chrome_options.binary_location='/opt/google/chrome/google-chrome'
-  driver = webdriver.Chrome(os.path.dirname(os.path.abspath(__file__))+"/../resources/chromedriver", chrome_options=chrome_options)
-  try:
-    driver.get("https://translate.google.com/#view=home&op=translate&sl=en&tl=hi")
-  except:
-      traceback.print_exc()
-      driver.close()
-      driver.quit()
-  #loop = asyncio.get_event_loop()
-  try:
+    for i in range(num_threads):
+        name='T-{i}'.format(i=i)
+        worker_thread = Thread(target=worker, args=(name, q))
+        worker_thread.start()
+        workers.append(worker_thread)
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    coro = loop.create_server(lambda:EchoServerClientProtocol(driver), '127.0.0.1', 8888)
+    for signame in ('SIGINT', 'SIGTERM'):
+      loop.add_signal_handler(getattr(signal, signame),
+                            functools.partial(handler, signame))
+    coro = loop.create_server(lambda:EchoServerClientProtocol(q), '127.0.0.1', 8888)
     server = loop.run_until_complete(coro)
     print('Serving on {}'. format(server.sockets[0].getsockname()))
     try:
-      loop.run_forever()
+      #loop.run_forever()
+      loop.run_until_complete(server.wait_closed())
+      print("Moving on")
     except KeyboardInterrupt:
       pass
 
     try:
-      driver.close()
-      driver.quit()
-      server.close()
-      loop.run_until_complete(server.wait_closed())
+      shutdown()
       loop.close()
+
     except:
       traceback.print_exc()
   except OSError:
       print('Quitting thread')
-      driver.close()
-      driver.quit()
+      for i in range(num_threads):
+          q.put('stop')
+      try:
+        while True:
+            worker_thread = workers.pop()
+            worker_thread.join()
+      except IndexError:
+        pass
+      traceback.print_exc()
 
 if __name__ == '__main__':
   main()
