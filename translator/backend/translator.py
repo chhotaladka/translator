847017008a0d3774814e20b1189b34dd26274fd9
anshulthakur <anshulthakur@rediffmail.com>
2019-09-21 20:02:50 +0530

    Alternate method for internet detection.

--- translator/backend/translator.py
+++ translator/backend/translator.py
@@ -4,7 +4,7 @@ from .keys import keys
 from django.core.cache import caches
 from translator.keying import _smart_key
 
-#import urllib.request
+import socket
 from nltk import tokenize
 
 cache = caches['default']
@@ -17,12 +17,27 @@ backends = {'dummy': 'dummy',
             'bing-selenium': 'bing-selenium',
             'nmt': 'nmt'}
 
+
+def check_internet(host="8.8.8.8", port=53, timeout=3):
+  """
+  Host: 8.8.8.8 (google-public-dns-a.google.com)
+  OpenPort: 53/tcp
+  Service: domain (DNS/TCP)
+  """
+  try:
+    socket.setdefaulttimeout(timeout)
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+    return True
+  except socket.error as ex:
+    print(ex)
+    return False
+
 class Engine(object):
   def __init__(self, engine='microsoft-api'):
-    #if engine is not 'nmt':
-    #  if (urllib.request.urlopen("https://google.com/").getcode() != 200):
-    #    print('Internet not working. Fallback to offile tool')
-    #    engine = 'nmt'
+    if engine is not 'nmt':
+      if (check_internet() == False):
+        print('Internet not working. Fallback to offile tool')
+        engine = 'nmt'
     self.backend = import_module('translator.backend.'+backends.get(engine, 'dummy')+'.api')
     self.client = self.backend.Client(key=keys.get(engine, None))
 
