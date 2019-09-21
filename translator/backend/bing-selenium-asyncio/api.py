ad8559467c71c6dbccfe23422a2e13d5b758f6a2
anshulthakur <anshulthakur@rediffmail.com>
2019-09-21 21:19:51 +0530

    Added bing asyncio backend

---
+++ translator/backend/bing-selenium-asyncio/api.py
@@ -0,0 +1,26 @@
+
+import traceback
+import time
+import os
+
+import socket
+from django.conf import settings
+
+#print('settings: ', settings.TRANSLATOR)
+
+HOST = settings.TRANSLATOR.get('OPTIONS').get('ip')
+PORT = settings.TRANSLATOR.get('OPTIONS').get('port')
+
+class Client(object):
+  def __init__(self, key=None, lazy=True):
+    self.key = key
+    self.translation = ''
+
+  def translate(self, text=None, src_lang='en', dst_lang='hi'):
+    if text is not None and len(text.strip()) > 0:
+      with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        sock.sendall(text.strip().encode())
+        self.translation = sock.recv(1024).decode()
+        sock.close()
+    return (self.translation)
---
+++ translator/backend/bing-selenium-asyncio/server.py
@@ -0,0 +1,192 @@
+import asyncio
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+import traceback
+import time
+import os
+
+import signal
+import functools
+
+
+server = None
+
+
+from queue import Empty, Queue
+from threading import Thread
+
+num_threads = 2
+
+q = None
+workers = []
+
+RELOAD_AFTER = 10
+
+def shutdown():
+  global q
+  global workers
+  global server
+
+  print('Shutting down...')
+  if len(workers)>0:
+      for i in range(num_threads):
+        q.put('stop')
+      try:
+        while True:
+            worker_thread = workers.pop()
+            worker_thread.join()
+      except IndexError:
+        pass
+
+
+  if server is not None:
+    server.close()
+    server = None
+    #loop.stop()
+    #loop.wait_closed()
+    #loop.close()
+
+def handler(signum):
+    print('Signal handled.')
+    shutdown()
+
+def translate(driver, text=None, wait=0.9):
+    if text is not None and len(text.strip()) > 0:
+      translation = ''
+      try:
+        src = self.driver.find_element_by_id("tta_input")
+        if src is not None:
+          src.clear();
+          src.send_keys(text)
+          time.sleep(wait)
+          dest = self.driver.find_element_by_id("tta_output")
+          translation = dest.get_attribute('value')
+        retry=False
+      except:
+          traceback.print_exc()
+          retry = True
+      #print(translation)
+      return (translation, retry)
+
+
+class EchoServerClientProtocol(asyncio.Protocol):
+  def __init__(self, queue, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.queue = queue
+    self.terminate = False
+
+  def connection_made(self, transport):
+    peername = transport.get_extra_info('peername')
+    print('Connection from {}'.format(peername))
+    self.transport = transport
+
+  def data_received(self, data):
+    print('data received')
+    self.message = data.decode()
+    print('Data received: {!r}'.format(self.message))
+    self.translation = ''
+    self.queue.put(self)
+
+  def send_response(self):
+    print('Send: {!r}'.format(self.translation))
+    self.transport.write(self.translation.encode('utf-8'))
+
+    print('Close the client socket')
+    self.transport.close()
+
+def worker(name, queue):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    retry_count = 0
+    #chrome_options.binary_location='/opt/google/chrome/google-chrome'
+    try:
+        driver = webdriver.Chrome(os.path.dirname(os.path.abspath(__file__))+"/../resources/chromedriver", chrome_options=chrome_options)
+        driver.get("https://www.bing.com/translator?from=en&to=hi")
+
+        while True:
+            try:
+                work = queue.get(timeout=0.1)
+                if isinstance(work, EchoServerClientProtocol):
+                    retry_count += 1
+                    (work.translation, retry )= translate(driver, work.message, wait=0.9)
+                    if(retry is True):
+                        driver.get("https://www.bing.com/translator?from=en&to=hi")
+                        (work.translation, retry )= translate(driver, work.message, wait=0.9)
+                    work.send_response()
+                    if(retry_count%RELOAD_AFTER == 0):
+                        retry_count = 0
+                        driver.get("https://www.bing.com/translator?from=en&to=hi")
+
+
+                elif isinstance(work, str):
+                    if work=='stop':
+                        break;
+                queue.task_done()
+            except Empty:
+                pass
+
+        print('Closing {a}'.format(a=name))
+        #driver.close()
+        driver.quit()
+
+    except:
+        traceback.print_exc()
+
+
+def main(ip='127.0.0.1', port=10000):
+  global loop
+  global server
+  global coro
+  global q
+  global workers
+
+  try:
+    q = Queue(num_threads)
+
+    for i in range(num_threads):
+        name='T-{i}'.format(i=i)
+        worker_thread = Thread(target=worker, args=(name, q))
+        worker_thread.start()
+        workers.append(worker_thread)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    for signame in ('SIGINT', 'SIGTERM'):
+      loop.add_signal_handler(getattr(signal, signame),
+                            functools.partial(handler, signame))
+    coro = loop.create_server(lambda:EchoServerClientProtocol(q), ip, port)
+    server = loop.run_until_complete(coro)
+    print('Serving on {}'. format(server.sockets[0].getsockname()))
+    try:
+      #loop.run_forever()
+      loop.run_until_complete(server.wait_closed())
+      print("Moving on")
+    except KeyboardInterrupt:
+      pass
+
+    try:
+      shutdown()
+      loop.close()
+
+    except:
+      traceback.print_exc()
+  except OSError:
+      print('Quitting thread')
+      for i in range(num_threads):
+          q.put('stop')
+      try:
+        while True:
+            worker_thread = workers.pop()
+            worker_thread.join()
+      except IndexError:
+        pass
+      traceback.print_exc()
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv)==2:
+    main(sys.argv[0], sys.argv[1])
+  else:
+    main()
